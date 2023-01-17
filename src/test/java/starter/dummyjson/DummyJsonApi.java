6b76488286fbb608dc0aa0360196a1b981c107ca
sucinm <sucinascaisar@gmail.com>
2023-01-17 22:08:23 +0700

    Add package StepDef

--- src/test/java/starter/dummyjson/DummyJsonApi.java
+++ src/test/java/starter/dummyjson/DummyJsonApi.java
@@ -6,6 +6,6 @@ public class DummyJsonApi {
     public static String POST_ADD_PRODUCT = Constant.BASE_URL + "/products?add";
     public static String PUT_UPDATE_PRODUCT = Constant.BASE_URL + "/products/{id}";
     public static String PATCH_UPDATE_PRODUCT = Constant.BASE_URL + "/products/{id}";
-    public static String DELETE_USER = Constant.BASE_URL + "/products/{id}";
+    public static String DELETE_PRODUCT = Constant.BASE_URL + "/products/{id}";
     public static String GET_LIST_PRODUCT = Constant.BASE_URL + "/auth/products";
 }
